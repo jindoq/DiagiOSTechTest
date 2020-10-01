5164919371a791f9bf9f306c6fe8c1fd31e4eb24
duytu93 <tutran@coinhako.com>
2020-10-01 10:50:01 +0700

    init project

---
+++ DiagiOSTechTest/DiagiOSTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiagiOSTechTest
+//
+//  Created by Duy Tu Tran on 10/1/20.
+//  Copyright © 2020 Duy Tu Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DiagiOSTechTest/Pods/Target Support Files/Pods-DiagiOSTechTest/Pods-DiagiOSTechTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiagiOSTechTest : NSObject
+@end
+@implementation PodsDummy_Pods_DiagiOSTechTest
+@end
